b91f915f4c6a749b8591e0ca021e9aa245685af4
Dustin Diaz <polvero@gmail.com>
2011-08-30 18:20:36 -0700

    - new style guidlines applied to main src

--- morpheus.js
+++ morpheus.js
@@ -5,71 +5,46 @@
   */
 !function (context, doc, win) {
 
-  var html = doc.documentElement,
-      rgbOhex = /^rgb\(|#/,
-      relVal = /^([+\-])=([\d\.]+)/,
-      numUnit = /^(?:[\+\-]=)?\d+(?:\.\d+)?(%|in|cm|mm|em|ex|pt|pc|px)$/,
-      // does this browser support the opacity property?
-      opasity = function () {
-        return typeof doc.createElement('a').style.opacity !== 'undefined';
-      }(),
+  var html = doc.documentElement
+    , rgbOhex = /^rgb\(|#/
+    , relVal = /^([+\-])=([\d\.]+)/
+    , numUnit = /^(?:[\+\-]=)?\d+(?:\.\d+)?(%|in|cm|mm|em|ex|pt|pc|px)$/
       // these elements do not require 'px'
-      unitless = { lineHeight: 1, zoom: 1, zIndex: 1, opacity: 1 },
-
+    , unitless = { lineHeight: 1, zoom: 1, zIndex: 1, opacity: 1 }
+      // does this browser support the opacity property?
+    , opasity = function () {
+        return typeof doc.createElement('a').style.opacity !== 'undefined'
+      }()
       // initial style is determined by the elements themselves
-      getStyle = doc.defaultView && doc.defaultView.getComputedStyle ?
+    , getStyle = doc.defaultView && doc.defaultView.getComputedStyle ?
         function (el, property) {
-          var value = null;
-          var computed = doc.defaultView.getComputedStyle(el, '');
-          computed && (value = computed[camelize(property)]);
-          return el.style[property] || value;
+          var value = null
+            , computed = doc.defaultView.getComputedStyle(el, '')
+          computed && (value = computed[camelize(property)])
+          return el.style[property] || value
         } : html.currentStyle ?
 
         function (el, property) {
-          property = camelize(property);
+          property = camelize(property)
 
           if (property == 'opacity') {
-            var val = 100;
+            var val = 100
             try {
-              val = el.filters['DXImageTransform.Microsoft.Alpha'].opacity;
+              val = el.filters['DXImageTransform.Microsoft.Alpha'].opacity
             } catch (e1) {
               try {
-                val = el.filters('alpha').opacity;
+                val = el.filters('alpha').opacity
               } catch (e2) {}
             }
-            return val / 100;
+            return val / 100
           }
-          var value = el.currentStyle ? el.currentStyle[property] : null;
-          return el.style[property] || value;
+          var value = el.currentStyle ? el.currentStyle[property] : null
+          return el.style[property] || value
         } :
-
         function (el, property) {
-          return el.style[camelize(property)];
-        },
-
-      rgb = function (r, g, b) {
-        return '#' + (1 << 24 | r << 16 | g << 8 | b).toString(16).slice(1);
-      },
-
-      // convert rgb and short hex to long hex
-      toHex = function (c) {
-        var m = /rgba?\((\d+),\s*(\d+),\s*(\d+)/.exec(c);
-        return (m ? rgb(m[1], m[2], m[3]) : c)
-        .replace(/#(\w)(\w)(\w)$/, '#$1$1$2$2$3$3'); // short to long
-      },
-
-      // change font-size => fontSize etc.
-      camelize = function (s) {
-        return s.replace(/-(.)/g, function (m, m1) {
-          return m1.toUpperCase();
-        });
-      },
-
-      fun = function (f) {
-        return typeof f == 'function';
-      },
-
-      frame = function () {
+          return el.style[camelize(property)]
+        }
+    , frame = function () {
         // native animation frames
         // http://webstuff.nfshost.com/anim-timing/Overview.html
         // http://dev.chromium.org/developers/design-documents/requestanimationframe-implementation
@@ -80,10 +55,32 @@
           win.msRequestAnimationFrame     ||
           function (callback) {
             win.setTimeout(function () {
-              callback(+new Date());
-            }, 10);
-          };
-      }();
+              callback(+new Date())
+            }, 10)
+          }
+      }()
+
+  function rgb(r, g, b) {
+    return '#' + (1 << 24 | r << 16 | g << 8 | b).toString(16).slice(1)
+  }
+
+  // convert rgb and short hex to long hex
+  function toHex(c) {
+    var m = /rgba?\((\d+),\s*(\d+),\s*(\d+)/.exec(c)
+    return (m ? rgb(m[1], m[2], m[3]) : c)
+      .replace(/#(\w)(\w)(\w)$/, '#$1$1$2$2$3$3') // short to long
+  }
+
+  // change font-size => fontSize etc.
+  function camelize(s) {
+    return s.replace(/-(.)/g, function (m, m1) {
+      return m1.toUpperCase()
+    })
+  }
+
+  function fun(f) {
+    return typeof f == 'function';
+  }
 
   /**
     * Core tween method that requests each frame
@@ -99,33 +96,32 @@
     ease = ease || function (t) {
       // default to a pleasant-to-the-eye easeOut (like native animations)
       return Math.sin(t * Math.PI / 2)
-    };
-    var time = duration || 1000,
-        diff = to - from,
-        start = +new Date(),
-        stop = 0,
-        end = 0;
-    frame(run);
+    }
+    var time = duration || 1000
+      , diff = to - from
+      , start = +new Date()
+      , stop = 0
+      , end = 0
+    frame(run)
 
     function run(t) {
       var delta = t - start;
       if (delta > time || stop) {
-        to = isFinite(to) ? to : 1;
-        stop ? end && fn(to) : fn(to);
-        done && done();
-        return;
+        to = isFinite(to) ? to : 1
+        stop ? end && fn(to) : fn(to)
+        return done && done()
       }
       // if you don't specify a 'to' you can use tween as a generic delta tweener
       // cool, eh?
       isFinite(to) ?
         fn((diff * ease(delta / time)) + from) :
-        fn(ease(delta / time));
-      frame(run);
+        fn(ease(delta / time))
+      frame(run)
     }
     return {
       stop: function (jump) {
-        stop = 1;
-        end = jump; // jump to end of animation?
+        stop = 1
+        end = jump // jump to end of animation?
       }
     }
   }
@@ -141,42 +137,42 @@
     * @return [x, y]
     */
   function bezier(points, pos) {
-    var n = points.length, r = [], i, j;
+    var n = points.length, r = [], i, j
     for (i = 0; i < n; ++i) {
-      r[i] = [points[i][0], points[i][1]];
+      r[i] = [points[i][0], points[i][1]]
     }
     for (j = 1; j < n; ++j) {
       for (i = 0; i < n - j; ++i) {
-        r[i][0] = (1 - pos) * r[i][0] + pos * r[parseInt(i + 1, 10)][0];
-        r[i][1] = (1 - pos) * r[i][1] + pos * r[parseInt(i + 1, 10)][1];
+        r[i][0] = (1 - pos) * r[i][0] + pos * r[parseInt(i + 1, 10)][0]
+        r[i][1] = (1 - pos) * r[i][1] + pos * r[parseInt(i + 1, 10)][1]
       }
     }
-    return [r[0][0], r[0][1]];
+    return [r[0][0], r[0][1]]
   }
 
   // this gets you the next hex in line according to a 'position'
   function nextColor(pos, start, finish) {
-    var r = [], i, e;
+    var r = [], i, e
     for (i = 0; i < 6; i++) {
-      from = Math.min(15, parseInt(start.charAt(i),  16));
-      to   = Math.min(15, parseInt(finish.charAt(i), 16));
-      e = Math.floor((to - from) * pos + from);
-      e = e > 15 ? 15 : e < 0 ? 0 : e;
-      r[i] = e.toString(16);
+      from = Math.min(15, parseInt(start.charAt(i),  16))
+      to   = Math.min(15, parseInt(finish.charAt(i), 16))
+      e = Math.floor((to - from) * pos + from)
+      e = e > 15 ? 15 : e < 0 ? 0 : e
+      r[i] = e.toString(16)
     }
-    return '#' + r.join('');
+    return '#' + r.join('')
   }
 
   // this retreives the frame value within a sequence
   function getTweenVal(pos, units, begin, end, k, i, v) {
     if (typeof begin[i][k] == 'string') {
-      return nextColor(pos, begin[i][k], end[i][k]);
+      return nextColor(pos, begin[i][k], end[i][k])
     } else {
       // round so we don't get crazy long floats
-      v = Math.round(((end[i][k] - begin[i][k]) * pos + begin[i][k]) * 1000) / 1000;
+      v = Math.round(((end[i][k] - begin[i][k]) * pos + begin[i][k]) * 1000) / 1000
       // some css properties don't require a unit (like zIndex, lineHeight, opacity)
-      !(k in unitless) && (v += units[i][k] || 'px');
-      return v;
+      if (!k in unitless) v += units[i][k] || 'px'
+      return v
     }
   }
 
@@ -185,7 +181,7 @@
     return (m = relVal.exec(val)) ?
       (i = parseFloat(m[2])) && (r = (start + i)) && m[1] == '+' ?
       r : start - i :
-      parseFloat(val);
+      parseFloat(val)
   }
 
   /**
@@ -203,17 +199,17 @@
     *     - this may also be a function that receives element to be animated. it must return a value
     */
   function morpheus(elements, options) {
-    var els = elements ? (els = isFinite(elements.length) ? elements : [elements]) : [], i,
-        complete = options.complete,
-        duration = options.duration,
-        ease = options.easing,
-        points = options.bezier,
-        begin = [],
-        end = [],
-        units = [],
-        bez = [],
-        originalLeft,
-        originalTop;
+    var els = elements ? (els = isFinite(elements.length) ? elements : [elements]) : [], i
+      , complete = options.complete
+      , duration = options.duration
+      , ease = options.easing
+      , points = options.bezier
+      , begin = []
+      , end = []
+      , units = []
+      , bez = []
+      , originalLeft
+      , originalTop
 
     delete options.complete;
     delete options.duration;
@@ -233,17 +229,17 @@
     for (i = els.length; i--;) {
 
       // record beginning and end states to calculate positions
-      begin[i] = {};
-      end[i] = {};
-      units[i] = {};
+      begin[i] = {}
+      end[i] = {}
+      units[i] = {}
 
       // are we 'moving'?
       if (points) {
 
-        var left = getStyle(els[i], 'left'),
-            top = getStyle(els[i], 'top'),
-            xy = [by(fun(originalLeft) ? originalLeft(els[i]) : originalLeft || 0, parseFloat(left)),
-                  by(fun(originalTop) ? originalTop(els[i]) : originalTop || 0, parseFloat(top))];
+        var left = getStyle(els[i], 'left')
+          , top = getStyle(els[i], 'top')
+          , xy = [by(fun(originalLeft) ? originalLeft(els[i]) : originalLeft || 0, parseFloat(left)),
+                  by(fun(originalTop) ? originalTop(els[i]) : originalTop || 0, parseFloat(top))]
 
         bez[i] = fun(points) ? points(els[i], xy) : points;
         bez[i].push(xy);
@@ -254,8 +250,8 @@
       }
 
       for (var k in options) {
-        var v = getStyle(els[i], k), unit,
-            tmp = fun(options[k]) ? options[k](els[i]) : options[k]
+        var v = getStyle(els[i], k), unit
+          , tmp = fun(options[k]) ? options[k](els[i]) : options[k]
         if (typeof tmp == 'string' &&
             rgbOhex.test(tmp) &&
             !rgbOhex.test(v)) {
@@ -271,7 +267,7 @@
           toHex(tmp).slice(1) :
           by(tmp, parseFloat(v));
         // record original unit
-        typeof tmp == 'string' && (unit = tmp.match(numUnit)) && (units[i][k] = unit[1]);
+        typeof tmp == 'string' && (unit = tmp.match(numUnit)) && (units[i][k] = unit[1])
       }
     }
     // ONE TWEEN TO RULE THEM ALL
@@ -280,27 +276,26 @@
       // fast for animating
       for (i = els.length; i--;) {
         if (points) {
-          xy = bezier(bez[i], pos);
-          els[i].style.left = xy[0] + 'px';
-          els[i].style.top = xy[1] + 'px';
+          xy = bezier(bez[i], pos)
+          els[i].style.left = xy[0] + 'px'
+          els[i].style.top = xy[1] + 'px'
         }
         for (var k in options) {
-          v = getTweenVal(pos, units, begin, end, k, i);
+          v = getTweenVal(pos, units, begin, end, k, i)
           k == 'opacity' && !opasity ?
             (els[i].style.filter = 'alpha(opacity=' + (v * 100) + ')') :
-            (els[i].style[camelize(k)] = v);
+            (els[i].style[camelize(k)] = v)
         }
       }
-    }, complete, ease);
+    }, complete, ease)
   }
 
   // expose useful methods
-  morpheus.tween = tween;
-  morpheus.getStyle = getStyle;
-  morpheus.bezier = bezier;
+  morpheus.tween = tween
+  morpheus.getStyle = getStyle
+  morpheus.bezier = bezier
 
-  typeof module !== 'undefined' && module.exports &&
-    (module.exports = morpheus);
-  context['morpheus'] = morpheus;
+  if (typeof module !== 'undefined') module.exports = morpheus
+  context['morpheus'] = morpheus
 
-}(this, document, window);
+}(this, document, window)
